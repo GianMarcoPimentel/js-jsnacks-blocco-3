d4bc727b87c936cdd5f55ffb8ff3fc54601bf93c
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-12 21:15:19 +0100

    ripristino esercizi

---
+++ jsnack5-bonus/js/script.js
@@ -0,0 +1,10 @@
+/* 
+Snack 5 (bonus):
+Scrivi una funzione che accetti una stringa come parametro 
+e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
+Es: 
+funzioneEsempio("nel mezzo del cammin di nostra vita")
+                "Nel Mezzo Del Cammin Di Nostra Vita"
+ */
+
+                
