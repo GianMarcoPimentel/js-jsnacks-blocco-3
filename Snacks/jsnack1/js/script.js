1fb4ab0a54a397588a269181e6900e81e548ac92
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-12 13:17:23 +0100

    Impostazione Snack1

---
+++ Snacks/jsnack1/js/script.js
@@ -0,0 +1,15 @@
+/* 
+Snack 1:
+Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+*/
+
+// chiedo all'utente di inserire una parola
+let parola = prompt("Inserisci una parola")
+
+function invertita() {
+    //memorizzo la parola inverita in una stringa vuota
+    let parolaInverita = "";
+     
+    //inizio un ciclo for dove l'iterzione inizia
+    
+}
