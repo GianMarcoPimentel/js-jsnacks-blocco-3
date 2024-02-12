88c0d45b031a73703e60c6c00c2097dfd1b60ae3
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-12 14:40:28 +0100

    Fine snack 1

--- Snacks/jsnack1/js/script.js
+++ Snacks/jsnack1/js/script.js
@@ -3,27 +3,7 @@ Snack 1:
 Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
 */
 
-// chiedo all'utente di inserire una parola
-/* let parola = "ciao"; */
-/* 
-
-//creo una funzione
-function invertita(parola) {
-
-    //inizio un ciclo for dove l'iterzione 
-    //inizierà dall'ultima parola fino alla prima
-    //dovrà essere maggiore o uguale a 0
-    //e avrà andamento decrescente
-    for( let i = parola.length - 1 ; i >= 0 ; i-- ){
-        parolaInverita = parolaInverita + parola[i];
-    }
-    
-    return parolaInverita;
-}
-
-let risultato = invertita(parola);
-document.getElementById("risultato").innerHTML = `${risultato}`; */
-
+// Inserisco una parola
 let parola = "ciao";
 parola = parola.split("")
 console.log(parola);
