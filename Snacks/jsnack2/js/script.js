0b3f0879dfadbe8f945656cc6f40a0a53d414e7a
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-12 16:32:28 +0100

    Inizio snack 2

---
+++ Snacks/jsnack2/js/script.js
@@ -0,0 +1,19 @@
+/* 
+Snack 2:
+Scrivi una funzione che fonda due array presi come parametri (date per scontato che abbiano lo stesso numero di elementi) 
+prendendo alternativamente gli elementi da uno e dall’altro
+es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+*/
+ //introduco i miei due array
+const array1 = [1, 2, 3,];
+const array2 = ["a", "b", "c"];
+const array3 = [];
+
+
+
+
+
+
+
+
+
